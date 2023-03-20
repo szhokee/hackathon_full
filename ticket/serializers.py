e81b7deb8282f24bbff30acd4f57ab6c9b4d9c4b
CaesarIman <bananad196@gmail.com>
2023-03-20 12:55:24 +0600

    comment

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
 from ticket.models import Ticket
+from ticket.tasks import send_ticket_confirmation_code
 
 class TicketSerializer(serializers.ModelSerializer):
     owner = serializers.EmailField(required=False)
     class Meta:
         model = Ticket
         fields = '__all__'
+
+    def create(self, validated_data):
+        amount = validated_data.get('amount')
+        event = validated_data.get('event')
+
+        if amount > event.amount:
+            raise serializers.ValidationError('Not enough tickets in event')
+        if amount == 0:
+            raise serializers.ValidationError('Tickets can not be 0')
+
+        event.amount -= amount
+        event.save(update_fields=['amount'])
+
+        ticket = Ticket.objects.create(**validated_data)
+
+        send_ticket_confirmation_code(ticket.owner.email, ticket.activation_code, ticket.event.title, ticket.total_price)
+
+        return ticket
--- ticket/urls.py
+++ ticket/urls.py
@@ -1,27 +1,11 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from ticket.views import *
+from django.urls import path, include
+from ticket.views import TicketOrderModelViewset, TicketConfirmAPIView
 
 router = DefaultRouter()
-router.register('apiview_get_post', TicketViewSet, basename='products')
-router.register('modelviewset_crud', TicketModelViewSet)
-router.register('product_mixin', TicketMixin)
-router.register('category', CategoryAPIView)
+router.register('', TicketOrderModelViewset)
 
 urlpatterns = [
-    path('func_get/', get_product),
-    path('func_post/', post_product),
-
-    path('generic_get/', TicketListGenericView.as_view()),
-    path('generic_post/', TicketCreateGenericView.as_view()),
-    path('generic_get_post/', TicketListCreateGenericView.as_view()),
-
-    path('apiview_get_post/', TicketAPIView.as_view()),
-
-    path('viewset_get_post/', TicketViewSet.as_view({'get':'list', 'post':'create'})),
-
-    path('hello/', get_hello),
-
+    path('confirm/<uuid:code>/', TicketConfirmAPIView.as_view()),
     path('', include(router.urls))
-
 ]
