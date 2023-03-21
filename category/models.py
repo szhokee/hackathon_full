9030b332f454bf345ec28ac9e92aec7d53912e58
szhokee <szhokee@gmail.com>
2023-03-21 20:01:32 +0600

    feeedbw

--- category/models.py
+++ category/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     title = models.SlugField(primary_key=True, unique=True)
-    # parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='categories', null=True, blank=True)
+    
  
     def __str__(self) -> str:
         return f'{self.title}'
