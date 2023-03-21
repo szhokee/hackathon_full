6ce8298ce2258f8c6c1779b9acdf3582d196212a
szhokee <szhokee@gmail.com>
2023-03-21 20:30:47 +0600

    feegg

--- category/models.py
+++ category/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Category(models.Model):
     title = models.SlugField(primary_key=True, unique=True)
 
- 
     def __str__(self) -> str:
         return f'{self.title}'
+
+#mudila
